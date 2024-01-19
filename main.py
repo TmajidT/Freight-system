6f401e7a94f743d03b340e4b4435d8c17210a9ec
Majid <majid.asfanani@gmail.com>
2024-01-19 18:14:40 +0330

    DONE! in about 3-4 hours :)))

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ import managers
 import orders
 
 def main():
+    print("welcome to this program :)")
+    while True:
+        user = int(input("Who Are You? enter 1 for customer , 2 for driver , 3 for manager or 9 to exit :"))
+        if user == 1:
+            customers.customers_panel()
+        elif user == 2:
+            drivers.driver_panel()
+        elif user == 3:
+            managers.manager_panel()
+        elif user == 9:
+            break
+
+
+    #quick access to the code :) just delete # in a section
 
     #customers.insert_customer(5,"majid sdf")
     #customers.display_all_customers()
@@ -20,7 +34,7 @@ def main():
 
     #customers.customers_panel()
     #drivers.driver_panel()
-    managers.manager_panel()
+    #managers.manager_panel()
 
 if __name__ == '__main__':
     main()
